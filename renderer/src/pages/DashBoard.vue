5ca2696f17a68a2042aa57f71cf7cc04ae8f9a28
chollamatsu <chonlamas.ch@gmail.com>
2025-09-28 17:09:01 +0700

    feat: [task-55] add css and components to use in the future

---
+++ renderer/src/pages/DashBoard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <h1>Hello user name account</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DashBoard",
+};
+</script>
+
+<style scoped>
+.container {
+  background-color: var(--dark-3);
+  backdrop-filter: blur(20px); /* glass blur */
+  -webkit-backdrop-filter: blur(20px);
+  width: 100%;
+  height: 100%;
+}
+</style>
