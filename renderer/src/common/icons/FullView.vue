b6a365a511ebb87863d846bde1414748222fe6ae
chollamatsu <chonlamas.ch@gmail.com>
2026-02-17 21:35:17 +0700

    style: [task-55] add svg icons

---
+++ renderer/src/common/icons/FullView.vue
@@ -0,0 +1,71 @@
+<template>
+  <svg
+    :width="`${width}px`"
+    :height="`${height}px`"
+    viewBox="0 0 24 24"
+    :fill="color"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect
+      x="4"
+      y="4"
+      width="6"
+      height="7"
+      rx="1"
+      stroke="#222222"
+      stroke-linejoin="round"
+    />
+    <rect
+      x="4"
+      y="15"
+      width="6"
+      height="5"
+      rx="1"
+      stroke="#222222"
+      stroke-linejoin="round"
+    />
+    <rect
+      x="14"
+      y="4"
+      width="6"
+      height="5"
+      rx="1"
+      stroke="#222222"
+      stroke-linejoin="round"
+    />
+    <rect
+      x="14"
+      y="13"
+      width="6"
+      height="7"
+      rx="1"
+      stroke="#222222"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "FullView",
+  props: {
+    width: {
+      type: Number,
+      require: false,
+      default: 10,
+    },
+    height: {
+      type: Number,
+      require: false,
+      default: 10,
+    },
+    color: {
+      type: String,
+      require: false,
+      default: "var(--dark-1)",
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ renderer/src/common/icons/PlusIocn.vue
@@ -0,0 +1,37 @@
+<template>
+  <svg
+    :width="`${width}px`"
+    :height="`${height}px`"
+    viewBox="0 0 24 24"
+    :fill="color"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M12 6L12 18" stroke="#222222" stroke-linecap="round" />
+    <path d="M18 12L6 12" stroke="#222222" stroke-linecap="round" />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "PlusIcon",
+  props: {
+    width: {
+      type: Number,
+      require: false,
+      default: 10,
+    },
+    height: {
+      type: Number,
+      require: false,
+      default: 10,
+    },
+    color: {
+      type: String,
+      require: false,
+      default: "var(--dark-1)",
+    },
+  },
+};
+</script>
+
+<style scoped></style>
